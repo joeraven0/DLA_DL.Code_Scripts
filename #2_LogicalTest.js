54a4205ab20176972fed2b1d0a2eb7f1415ed2f1
Joe Raven <joakim.ringstad.raavi@gmail.com>
2020-04-30 13:23:15 +0200

    Update #2_LogicalTest.js

--- #2_LogicalTest.js
+++ #2_LogicalTest.js
@@ -5,7 +5,7 @@ function onResult(results, output) {
     
     //If code found within reading phase
     if(results.redingCount>0){          
-        var msg = results.codes[0].content;   //First code in list (No iteration if more than one code found!)
+        var msg = results.codes[0].content;   //Get barcode content from first code in list(No iteration if more than one code found!)
         var strMsg = msg[msg.length-2];       //Keep second last char
         strMsg += msg[msg.length-1];          //Keep last char and add to string
         if(strMsg=="NO"){                     //If two last chars are "NO", replace the output variable
