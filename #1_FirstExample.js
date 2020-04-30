5d4da1251dad372f870f88078f5e36097f7a9794
Joe Raven <joakim.ringstad.raavi@gmail.com>
2020-04-30 13:21:31 +0200

    Update #1_FirstExample.js

--- #1_FirstExample.js
+++ #1_FirstExample.js
@@ -1,16 +1,5 @@
 function onResult(results, output) {
-    
-    var prefix = "\x02";                      //Prefix <STX>=\x02
-    var suffix = "";
-    
-    //If code found within reading phase
-    if(results.redingCount>0){          
-        var msg = results.codes[0].content;   //First code in list (No iteration if more than one code found!)
-        var strMsg = msg[msg.length-2];       //Keep second last char
-        strMsg += msg[msg.length-1];          //Keep last char and add to string
-        if(strMsg=="NO"){                     //If two last chars are "NO", replace the output variable
-            msg = strMsg;
-        }    
-        output.setMessage(prefix+msg+suffix)  //Send to output
+    if(results.redingCount>0){                              //If barcode found within reading phase
+        output.setMessage("BARCODE FOUND!")                 //Send to output
     }
 }
