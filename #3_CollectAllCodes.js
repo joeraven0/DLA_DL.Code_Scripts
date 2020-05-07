4b63c346d75bcc90acc79f2552acf1edf15a1515
Joe Raven <joakim.ringstad.raavi@gmail.com>
2020-05-07 15:03:53 +0200

    Update #3_CollectAllCodes.js

--- #3_CollectAllCodes.js
+++ #3_CollectAllCodes.js
@@ -3,6 +3,7 @@ function onResult(result, output) {
     var CR = "\x0D";
     var LF = "\x0A";
     //For loop to iterate through codes
+    
     for(var j = 0; j<result.codes.length; j++){
     //Add cam slave id and code content to string
 		outputMessage+= "CAMid: " + result.codes[j].addresses + "   Content:" + result.codes[j].content + CR;
